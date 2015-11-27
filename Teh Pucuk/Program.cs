d681bbd586ae710d67fcaefab2c60f30cc0ab834
argosptr <argosptr@gmail.com>
2015-11-27 15:59:43 +0700

    asa

--- Teh Pucuk/Program.cs
+++ Teh Pucuk/Program.cs
@@ -12,6 +12,7 @@ namespace TehPucuk
 {
     internal class Program
     {
+        bool spam = false;
         private static bool ownTowers = true;
         private static bool enemyTowers = true;
         private static bool jarSer = true;
@@ -70,7 +71,7 @@ namespace TehPucuk
 
 
             //Aura Keliatan di map
-            bool spam = false;
+
             var player = ObjectMgr.LocalPlayer;
             var units = ObjectMgr.GetEntities<Unit>().Where(
             x =>
